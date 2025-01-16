797e3167f34766508fa8d3f4a6ddbd21dbf16cd5
Ben Lee <ben@ben.cm>
2025-01-15 16:20:54 -0800

    Update rules_kotlin to 2.1.0

--- src/worker/InvocationWorker.kt
+++ src/worker/InvocationWorker.kt
@@ -4,7 +4,6 @@ internal class InvocationWorker(
   private val args: Array<String>,
   private val workerMessageProcessor: Worker.WorkRequestCallback,
 ) : Worker {
-
   override fun processRequests(): Int {
     // Handle a single work request
     return workerMessageProcessor.processWorkRequest(args.toList(), System.err)
